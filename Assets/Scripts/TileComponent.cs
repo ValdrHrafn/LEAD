48f8917db9a4af45ee20413413ca9264fdf4b23c
Andries <61500390+Emerald001@users.noreply.github.com>
2022-09-09 13:10:31 +0200

    Added WFC

---
+++ Assets/Scripts/TileComponent.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileComponent : MonoBehaviour
+{
+    public Vector2 xAs = new Vector2();
+    public Vector2 yAs = new Vector2();
+    public Vector2 zAs = new Vector2();
+}
